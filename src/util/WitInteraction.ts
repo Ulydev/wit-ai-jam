d477d23ffb960b4c10f9746c48eedd9dd212643e
ulydev <ulydev@live.fr>
2020-10-30 10:38:24 +0100

    add proxy to env file

--- src/util/WitInteraction.ts
+++ src/util/WitInteraction.ts
@@ -3,15 +3,13 @@ import { BrowserController, Command } from "./BrowserController"
 export class WitInteraction {
 
     static API_KEY = process.env.REACT_APP_WIT_AI_API_KEY
-    static withCors = (s: string) => `http://localhost:8080/${s}`
+    static withCors = (s: string) => `${process.env.REACT_APP_PROXY}/${s}`
     static API_SPEECH_URI = "https://api.wit.ai/speech"
 
     static processAudio = async (blob: Blob) => {
         const response = await fetch(WitInteraction.withCors(WitInteraction.API_SPEECH_URI), {
             method: "POST",
             headers: {
-                "Origin": "*",
-                "Access-Control-Allow-Origin": "*",
                 "Authorization": `Bearer ${WitInteraction.API_KEY}`,
                 "Content-Type": "audio/wav",
                 //"Transfer-Encoding": "chunked"
@@ -21,10 +19,6 @@ export class WitInteraction {
         const json = await response.json()
         const command: Command = { text: json["text"], intents: json["intents"], entities: json["entities"] }
         return command
-    }
-
-    static executeCommand = async (command: Command) => {
-        await BrowserController.executeCommand(command)
     }
 
 }
